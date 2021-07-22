5ff3c8d3f098d1e477e4ae08d504874be6722461
Stefan Pop <stefan9161@gmail.com>
2021-07-22 22:28:51 +0200

    imported the previously created components into the entry js file

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 
+// Importing the Start and Chat components
+import Start from './components/Start';
+import Chat from './components/Chat';
+
 export default class HelloWorld extends Component {
 	constructor(props) {
 		super(props);
