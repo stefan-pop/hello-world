131538c7c13d919a0e46d128b5e448b4b22d720e
Stefan Pop <stefan9161@gmail.com>
2021-07-23 15:49:11 +0200

    updated Chat component

--- components/Chat.js
+++ components/Chat.js
@@ -2,13 +2,31 @@ import React, {Component} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: this.props.route.params.name,
+            bgColor: this.props.route.params.bgColor
+        }
+    }
+
+    componentDidMount() {
+        this.props.navigation.setOptions({title: !this.state.name ? 'Chat' : this.state.name })
+    }
+
     render() {
+        const {bgColor} = this.state;
+
         return (
-            <View>
+            <View style={[styles.container, {backgroundColor: bgColor}]}>
                 <Text>Chat Component</Text>
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
