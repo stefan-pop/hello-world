1c9f2237e0d506623f3b7fd1a0251df778857ea0
Stefan Pop <stefan9161@gmail.com>
2021-08-03 21:26:16 +0200

    styled the component

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -133,6 +133,28 @@ export default class CustomActions extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        width: 26,
+        height: 26,
+        marginLeft: 10,
+        marginBottom: 10,
+    },
+    wrapper: {
+        borderRadius: 13,
+        borderColor: '#b2b2b2',
+        borderWidth: 2,
+        flex: 1,
+    },
+    iconText: {
+        color: '#b2b2b2',
+        fontWeight: 'bold',
+        fontSize: 16,
+        backgroundColor: 'transparent',
+        textAlign: 'center',
+    },
+});
+
 // Define the prop types
 CustomActions.contextTypes = {
     actionSheet: PropTypes.func,
