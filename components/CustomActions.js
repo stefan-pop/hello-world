03f16ddf9b3691d5c886f7a11003613693f80168
Stefan Pop <stefan9161@gmail.com>
2021-08-03 21:21:59 +0200

    imports for the "CustomActions" component

---
+++ components/CustomActions.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
+
+// Import Firebase
+import firebase from 'firebase';
+
